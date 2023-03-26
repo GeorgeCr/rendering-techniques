87496652d0ecd142f82cf70f4e98bceab0cbcd2b
George Craciun-Vasilache <georgecraciun1998@gmail.com>
2023-03-26 20:09:31 +0300

    Add webpack

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Welcome</div>;
+  return <div>Welcome <button onClick={() => console.log("clicked")}>Click</button></div>;
 };
 
 export default App;
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,18 @@
+import path from "path";
 import express from "express";
+import ssr from "./renderer";
 
 const app = express();
+// console.log(path.join(__dirname, "..", "public"), "dir")
+app.use(express.static("public"));
 
 app.get("/health", (_req, res) => {
   res.send("OK");
+});
+
+app.get("*", (req, res) => {
+  const page = ssr();
+  res.send(page);
 });
 
 const PORT = process.env.PORT || 3000;
